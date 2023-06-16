67adbbbd5c5fe2e55a1a68f1b9cefe9a251509d2
charles-matu <matucharleswachira@gmail.com>
2023-06-16 19:32:06 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *Return: 0 (Success)
+*/
+int main(void)
+{
+	int x = 97;
+	int y = 65;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	while (y <= 90)
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
