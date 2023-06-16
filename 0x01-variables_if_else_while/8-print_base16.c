8d6ad2cd0566a3ed76e18e71c923364b17acc789
charles-matu <matucharleswachira@gmail.com>
2023-06-16 20:41:23 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j <= 102; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
+
