baf190c448cacd8fed3d82d1d3fea6164da525d1
charles-matu <matucharleswachira@gmail.com>
2023-06-16 19:15:03 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *Return: 0 (Success)
+*/
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
