c226a20d0d802e99602ead154941cc9ed722b429
charles-matu <matucharleswachira@gmail.com>
2023-06-16 21:35:24 +0300

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; j <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
