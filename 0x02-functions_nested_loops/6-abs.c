235b16552aa5708d12b2224d8dd5713f3c117299
charles-matu <matucharleswachira@gmail.com>
2023-06-20 19:56:18 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,14 +5,11 @@
  * @n: the int to check
  * Return: the absolute value of int
  */
-int _abs(int i)
+int _abs(int n)
 {
-	if (i >= 0)
+	if (n >= 0)
 	{
-		return (1);
+		return (n);
 	}
-	else
-	{
-		return (i * -1);
-	}
+	return (-n);
 }
