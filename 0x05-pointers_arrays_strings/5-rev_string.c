df859ecc9bfca9cae56a305b0ff479f1887a8bf3
charles-matu <matucharleswachira@gmail.com>
2023-06-27 01:23:30 +0300

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,25 +6,23 @@
  */
 void rev_string(char *s)
 {
-        int length = 0;
-        int start = 0;
-        int end = 0;
-        char temp;
+	int length = 0;
+	int start = 0;
+	int end = 0;
+	char temp;
 
-    /* Calculate the length of the string */
-        while (s[length] != '\0')
-        {
-                length++;
-        }
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    /* Reverse the string */
-        end = length - 1;
-        while (start < end)
-        {
-                temp = s[start];
-                s[start] = s[end];
-                s[end] = temp;
-                start++;
-                end--;
-        }
+	end = length - 1;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
 }
