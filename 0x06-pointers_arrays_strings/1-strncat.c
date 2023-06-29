523c002da2883296f02219248cd597db07a905e4
Your Name <alvinkim2018@gmail.com>
2023-06-29 14:08:46 -0700

    ./1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,17 +9,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int x, z;
-x = 0;
-z = 0;
-while (dest[x] != '\0')
-x++;
-while (src[z] != '\0' && z < n)
-{
-dest[x] = src[z];
-x++;
-x++;
-}
-dest[z] = '\0';
-return (dest);
+	int x, z;
+
+	x = 0;
+	z = 0;
+	while (dest[x] != '\0')
+		x++;
+	while (src[z] != '\0' && z < n)
+	{
+		dest[x] = src[z];
+		x++;
+		x++;
+	}
+	dest[z] = '\0';
+	return (dest);
 }
