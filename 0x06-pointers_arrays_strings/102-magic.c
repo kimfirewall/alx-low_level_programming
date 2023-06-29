488d40f54bcf9288ca5bec69f449c5f16225463b
Your Name <alvinkim2018@gmail.com>
2023-06-29 13:04:07 -0700

    pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,46 +1,23 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * print_buffer - Prints the content of a buffer
- * @b: The buffer to print
- * @size: The number of bytes to print
- */
-void print_buffer(char *b, int size)
+int main(void)
 {
-	int i, j;
+  int n;
+  int a[5];
+  int *p;
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
-
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
-				printf("%02x", (unsigned char)b[j]);
-			else
-				printf("  ");
-
-			if (j % 2 != 0)
-				printf(" ");
-		}
-
-		for (j = i; j < i + 10; j++)
-		{
-			if (j >= size)
-				break;
-
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]);
-			else
-				printf(".");
-		}
-		printf("\n");
-	}
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
